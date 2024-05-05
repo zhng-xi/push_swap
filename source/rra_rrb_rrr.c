63eed24366e579d32f16c032256752fd07c830bc
Xi Zheng <xzheng@c3r8p1.42vienna.com>
2024-05-05 18:20:06 +0200

    modified rotate

--- source/rra_rrb_rrr.c
+++ source/rra_rrb_rrr.c
@@ -6,7 +6,7 @@
 /*   By: xzheng <xzheng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 16:44:29 by xzheng            #+#    #+#             */
-/*   Updated: 2024/05/04 13:38:33 by xzheng           ###   ########.fr       */
+/*   Updated: 2024/05/05 18:19:28 by xzheng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,16 @@
 void	rra(t_stack **stack_a, int print)
 {
 	t_stack	*tmp;
-	int		i;
+	t_stack	*last;
+	t_stack	*top;
 
-	if (!*stack_a || !(*stack_a)->next)
+	if (!(*stack_a) || !(*stack_a)->next)
 		return ;
-	i = 0;
-	tmp = *stack_a;
-	while ((*stack_a)->next)
-	{
-		*stack_a = (*stack_a)->next;
-		i++;
-	}
-	(*stack_a)->next = tmp;
-	while (i > 1)
-	{
-		tmp = tmp->next;
-		i--;
-	}
+	tmp = (*stack_a);
+	last = find_last(*stack_a);
+	top = (*stack_a)->next;
 	tmp->next = NULL;
+	(*stack_a) = top;
 	if (print == 0)
 		ft_putstr_fd("rra\n", 1);
 }
@@ -40,24 +32,16 @@ void	rra(t_stack **stack_a, int print)
 void	rrb(t_stack **stack_b, int print)
 {
 	t_stack	*tmp;
-	int		i;
+	t_stack	*last;
+	t_stack	*top;
 
-	if (!*stack_b || !(*stack_b)->next)
+	if (!(*stack_b) || !(*stack_b)->next)
 		return ;
-	i = 0;
-	tmp = *stack_b;
-	while ((*stack_b)->next)
-	{
-		*stack_b = (*stack_b)->next;
-		i++;
-	}
-	(*stack_b)->next = tmp;
-	while (i > 1)
-	{
-		tmp = tmp->next;
-		i--;
-	}
+	tmp = (*stack_b);
+	last = find_last(*stack_b);
+	top = (*stack_b)->next;
 	tmp->next = NULL;
+	(*stack_b) = top;
 	if (print == 0)
 		ft_putstr_fd("rrb\n", 1);
 }
